2ea62b10c9e478ac8fe413bc9962703e8f2f8089
Mauricio Rodrigues <mmauricio.vsr@gmail.com>
2018-07-14 13:21:47 -0300

    added support to react routes

--- docs/components/App.jsx
+++ docs/components/App.jsx
@@ -1,23 +1,26 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Introduction from "./Introduction";
 import Sidebar from "./Sidebar";
 
 class App extends React.Component {
     render() {
         return (
-            <div className="my-3 my-md-5">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-lg-9">
-                            <Introduction/>
-                        </div>
-                        <div className="col-lg-3">
-                            <Sidebar />
+            <Router>
+                <div className="my-3 my-md-5">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-lg-9">
+                                <Route exact path="/introduction" component={Introduction} />
+                            </div>
+                            <div className="col-lg-3">
+                                <Sidebar />
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            </Router>
         );
     }
 }
--- docs/components/Sidebar.jsx
+++ docs/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Sidebar extends React.Component {
     render() {
@@ -8,21 +9,48 @@ class Sidebar extends React.Component {
                     <i className="fe fe-github mr-2"></i>Browse source code
                 </a>
 
-                <div className="list-group list-group-transparent mb-0">
-                    <a href="../docs/index.html" className="list-group-item list-group-item-action active"><span className="icon mr-3"><i className="fe fe-flag"></i></span>Introduction</a>
-                </div>
-
-                <div className="list-group list-group-transparent mb-0">
-                    <a href="../docs/alerts.html" className="list-group-item list-group-item-action"><span className="icon mr-3"><i className="fe fe-alert-triangle"></i></span>Alerts</a>
-                    <a href="../docs/avatars.html" className="list-group-item list-group-item-action"><span className="icon mr-3"><i className="fe fe-user"></i></span>Avatars</a>
-                    <a href="../docs/buttons.html" className="list-group-item list-group-item-action"><span className="icon mr-3"><i className="fe fe-plus-square"></i></span>Buttons</a>
-                    <a href="../docs/colors.html" className="list-group-item list-group-item-action"><span className="icon mr-3"><i className="fe fe-feather"></i></span>Colors</a>
-                    <a href="../docs/cards.html" className="list-group-item list-group-item-action"><span className="icon mr-3"><i className="fe fe-image"></i></span>Cards</a>
-                    <a href="../docs/charts.html" className="list-group-item list-group-item-action"><span className="icon mr-3"><i className="fe fe-pie-chart"></i></span>Charts</a>
-                    <a href="../docs/form-components.html" className="list-group-item list-group-item-action"><span className="icon mr-3"><i className="fe fe-check-square"></i></span>Form components</a>
-                    <a href="../docs/tags.html" className="list-group-item list-group-item-action"><span className="icon mr-3"><i className="fe fe-tag"></i></span>Tags</a>
-                    <a href="../docs/typography.html" className="list-group-item list-group-item-action"><span className="icon mr-3"><i className="fe fe-type"></i></span>Typography</a>
-                </div>
+                <ul className="list-group list-group-transparent mb-0">
+                    <li data-href="../docs/index.html" className="list-group-item list-group-item-action active">
+                        <span className="icon mr-3"><i className="fe fe-flag"></i></span>
+                        <Link to="/introduction">Introduction</Link>
+                    </li>
+                    <li data-href="../docs/alerts.html" className="list-group-item list-group-item-action">
+                        <span className="icon mr-3"><i className="fe fe-alert-triangle"></i></span>
+                        <Link to="/alerts">Alerts</Link>
+                    </li>
+                    <li data-href="../docs/avatars.html" className="list-group-item list-group-item-action">
+                        <span className="icon mr-3"><i className="fe fe-user"></i></span>
+                        <Link to="/avatars">Avatars</Link>
+                    </li>
+                    <li data-href="../docs/buttons.html" className="list-group-item list-group-item-action">
+                        <span className="icon mr-3"><i className="fe fe-plus-square"></i></span>
+                        <Link to="/buttons">Buttons</Link>
+                    </li>
+                    <li data-href="../docs/colors.html" className="list-group-item list-group-item-action">
+                        <span className="icon mr-3"><i className="fe fe-feather"></i></span>
+                        <Link to="/colors">Colors</Link>
+                    </li>
+                    <li data-href="../docs/cards.html" className="list-group-item list-group-item-action">
+                        <span className="icon mr-3"><i className="fe fe-image"></i></span>
+                        <Link to="/cards">Cards</Link>
+                    </li>
+                    <li data-href="../docs/charts.html" className="list-group-item list-group-item-action">
+                        <span className="icon mr-3"><i className="fe fe-pie-chart"></i></span>
+                        <Link to="/charts">Charts</Link>
+                    </li>
+                    <li data-href="../docs/form-components.html" className="list-group-item list-group-item-action">
+                        <span className="icon mr-3"><i className="fe fe-check-square"></i></span>
+                        <Link to="/form-components">Form components</Link>
+                    </li>
+                    <li data-href="../docs/tags.html" className="list-group-item list-group-item-action">
+                        <span className="icon mr-3"><i className="fe fe-tag"></i></span>
+                        <Link to="/tags">Tags</Link>
+                    </li>
+                    <li data-href="../docs/typography.html" className="list-group-item list-group-item-action">
+                        <span className="icon mr-3"><i className="fe fe-type"></i></span>
+                        <Link to="/typography">Typography</Link>
+                    </li>
+                </ul>
                 <div className="d-none d-lg-block mt-6">
                     <a href="https://github.com/tabler/tabler/edit/dev/src/_docs/index.md" className="text-muted">Edit this page</a>
                 </div>
