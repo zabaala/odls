38493358eef490ff3b77b18e1059f37d3f7e521a
Mauricio Rodrigues <mmauricio.vsr@gmail.com>
2018-07-14 17:43:22 -0300

    created Chart component

---
+++ docs/components/Charts.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Charts extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2 className="mt-0 mb-4">Charts</h2>
+                <p>WIP...</p>
+            </div>
+        );
+    }
+}
+
+export default Charts;
